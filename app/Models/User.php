3f2f9d7d3d51e4b5bb2231ec99ede960075f0a3f
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-15 23:12:12 +0600

    hasRole added

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,4 +61,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Rental::class);
     }
+
+    public function hasRole($role)
+    {
+        return $this->role === $role;
+    }
 }
