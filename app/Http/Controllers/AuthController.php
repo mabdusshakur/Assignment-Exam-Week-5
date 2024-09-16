04e747a061a7228304149811877632a6dddeaf71
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-16 17:43:26 +0600

    auth controller added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function loginPage()
+    {
+        return view('auth.login');
+    }
+
+    public function registerPage()
+    {
+        return view('auth.register');
+    }
+
+    public function loginUser(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function registerUser(Request $request)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:6', 'confirmed'],
+        ]);
+
+        $user = User::create($data);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
+    public function logoutUser()
+    {
+        Auth::logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        return redirect('/');
+    }
+}
