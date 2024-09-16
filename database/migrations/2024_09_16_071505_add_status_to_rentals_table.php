b98cb6924744f1916d5774feca4145049aa8d1ef
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-09-16 13:16:01 +0600

    drop added for status

--- database/migrations/2024_09_16_071505_add_status_to_rentals_table.php
+++ database/migrations/2024_09_16_071505_add_status_to_rentals_table.php
@@ -21,7 +21,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('rentals', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
         });
     }
 };
